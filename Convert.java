a27ed3970de25e0e44cf234347fb7e71970ddb97
Rishabh Mudgal <62346513+mudgalrishabh18@users.noreply.github.com>
2020-06-24 10:49:18 +0530

    Add files via upload

---
+++ Convert.java
@@ -0,0 +1,17 @@
+package flowcontrol;
+
+import java.util.Scanner;
+
+public class Convert {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any character:");
+		String c=sc.nextLine();
+		sc.close();
+		Character c=s;
+		System.out.println(Character.toLowerCase);
+
+	}
+
+}
